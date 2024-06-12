9725b2df935ba09447e8d2b71fd9ba5d6f678813
petar.krstic <petar.krstic@frontmendev.com>
2024-06-12 10:03:27 +0200

    Fixed toptenner bat

--- CopyAssetsTopTenner.bat
+++ CopyAssetsTopTenner.bat
@@ -2,4 +2,4 @@ SET destinationPath=C:\RealisticGames\toptenner
 SET sourcePathSpine=W:\KL_Workshop\Pr_Top Tenner\_____FINAL_CONTENT\spine\_output
 SET sourcePathMotion=W:\KL_Workshop\Pr_Top Tenner\_____FINAL_CONTENT\Motion\_output
 
-Powershell.exe -noexit -executionpolicy bypass -command "& './CopyAssets.ps1' -DestinationPath '%destinationPath%' -SourcePath '%sourcePathSpine%' -SourcePathSecond '%sourcePathMotion%'"
+Powershell.exe -noexit -executionpolicy bypass -command "& './CopyAssets.ps1' -DestinationPath '%destinationPath%' -SourcePath '%sourcePathMotion%' -SourcePathSecond '%sourcePathSpine%'"
